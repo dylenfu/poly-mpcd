e840e027b8bec0eeec7e82165cd35f90e7331b7b
dylenfu <dylenfu@126.com>
2020-07-30 18:43:01 +0800

    add custom ecdsa.signature

---
+++ tss/ecdsa.go
@@ -0,0 +1,34 @@
+// todo(fuk): modify package description
+// Package ecdsa defines interfaces for ECDSA signing based on [SEC 1].
+//
+//   [SEC 1]: Standards for Efficient Cryptography, SEC 1: Elliptic Curve
+//     Cryptography, Certicom Research, https://www.secg.org/sec1-v2.pdf
+package tss
+
+import (
+	xecdsa "crypto/ecdsa"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+// PublicKey holds a public key of the Signer.
+type PublicKey xecdsa.PublicKey
+
+// Marshal serializes Public Key to bytes in uncompressed form as described in
+// [SEC 1] section 2.3.3: `04 + <x coordinate> + <y coordinate>`
+func (pk *PublicKey) Marshal() []byte {
+	return crypto.FromECDSAPub((*xecdsa.PublicKey)(pk))
+}
+
+// Signature holds a signature in a form of two big.Int `r` and `s` values and a
+// recovery ID value in {0, 1, 2, 3}.
+//
+// The signature is chain-agnostic. Some chains (e.g. Ethereum and BTC) requires
+// `v` to start from 27. Please consult the documentation about what the
+// particular chain expects.
+type Signature struct {
+	R          *big.Int
+	S          *big.Int
+	RecoveryID int
+}
